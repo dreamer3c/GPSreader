8594c92dfcd9ec697b6b4526a64b907d33814de6
dreamer3c <2978425474@qq.com>
2017-06-22 09:24:14 +0800

    20161104579
    new

---
+++ GPSreader/Header.h
@@ -0,0 +1,20 @@
+//
+//  Header.h
+//  GPSreader
+//
+//  Created by 20161104579 on 2017/6/22.
+//  Copyright © 2017年 20161104579. All rights reserved.
+//
+#include<stdio.h>
+#include<>
+typedef struct GPS
+{
+    char UCTtime;
+    char location;
+    char longitude;
+    char latitude;
+    char UCTdayandmonth;
+    char speed;
+    char orientation;
+    char
+}GPRMC;
---
+++ GPSreader/function.c
@@ -0,0 +1,9 @@
+//
+//  function.c
+//  GPSreader
+//
+//  Created by 20161104579 on 2017/6/22.
+//  Copyright © 2017年 20161104579. All rights reserved.
+//
+
+#include <Header.h>
